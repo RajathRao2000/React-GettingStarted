579c63d1c6f4907846688b9d5c395d5c5bfb3354
Rajath Rao <raorajath1998@gmail.com>
2024-02-03 21:42:46 +0530

    first commit

---
+++ src/components/ExpenceItem.js
@@ -0,0 +1,13 @@
+function ExpenceItem() {
+  return (
+    <div>
+      <h1>Expence item!</h1>
+      <p>Expense Items</p>
+      <p>Food Rs 10</p>
+      <p>Petrol Rs 100</p>
+      <p>Movies Rs 200</p>
+    </div>
+  );
+}
+
+export default ExpenceItem;
